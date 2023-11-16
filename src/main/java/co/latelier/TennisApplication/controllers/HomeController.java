5a8e96b8ef3086bb72404db3cf2a5e023870b9bc
Ksenia Shults <ksenija.schulz@gmail.com>
2023-11-16 22:33:39 +0100

    Added information about endpoints to the main page of the application

--- src/main/java/co/latelier/TennisApplication/controllers/HomeController.java
+++ src/main/java/co/latelier/TennisApplication/controllers/HomeController.java
@@ -7,6 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String home() {
-        return "Welcome to the Tennis Players API!";
+        return "<h2>API Usage</h2>" +
+                "<p>The application exposes several RESTful endpoints. Here are some examples of usage:</p>" +
+                "<ul>" +
+                "<li>Get all players: <code>/api/players</code></li>" +
+                "<li>Get a player by ID: <code>/api/players/{id}</code></li>" +
+                "<li>Get statistics: <code>/api/statistics</code></li>" +
+                "</ul>" +
+                "<p>The last endpoint returns statistics such as the country with the highest win ratio, the average BMI of players, and the median height of players.</p>";
     }
 }
